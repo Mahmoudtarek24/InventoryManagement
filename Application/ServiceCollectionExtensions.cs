7896d1fb1d5c597b75f8cbbaa732ccdcd6eb3d90
mahmoud tarek <mahmoudtark556@gmail.com>
2025-06-16 18:58:08 +0300

    Add project files.

---
+++ Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection service, IConfiguration configuration)
+		{
+
+			return service;
+		}
+	}
+}
